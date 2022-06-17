f1b021cdc81de21f5895f27fff84e9df88e83f50
loganwu <loganwu365@gmail.com>
2022-06-17 14:13:41 +0800

    完成路由，创建导航栏

---
+++ src/views/Statistics.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>Statistics</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    h3 {
+        color: skyblue;
+    }
+</style>
