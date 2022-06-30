7f6a9990064a9c1073b1ef8b62a79915ea9acfd0
loganwu <loganwu365@gmail.com>
2022-06-30 23:58:35 +0800

    完成Money.vue-Notes组件TS双向绑定

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,15 +2,19 @@
   <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" placeholder="在这里输入备注">
+      <input type="text" placeholder="在这里输入备注" v-model="noteContent">
     </label>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Notes'
-  };
+  import Vue from 'vue'
+  import { Component } from 'vue-property-decorator'
+  
+  @Component
+  export default class Notes extends Vue{
+   noteContent = '';
+  }
 </script>
 
 <style lang="scss" scoped>
