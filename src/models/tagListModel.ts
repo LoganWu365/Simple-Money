88bcd3a7cc5cfc86627b2a58dbde949bcd098dbe
loganwu <loganwu365@gmail.com>
2022-07-24 21:12:49 +0800

    可以编辑和删除标签

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -7,6 +7,8 @@ type TagListModel = {
     data: Tag[],
     fetch:() => Tag[],
     create: (name:string) => 'success' | 'duplicated' | 'null',//联合类型
+    update: (id:string,name:string) => string,
+    remove: (id:string) => string,
     save:() => void
 }
 const tagListModel: TagListModel = {
@@ -24,6 +26,34 @@ const tagListModel: TagListModel = {
             name: input
         }
         this.data.push(newTag);
+        this.save();
+        return 'success';
+    },
+    update(id:string,name:string){
+        const idList = this.data.map(tag => tag.id);
+        if(idList.indexOf(id) >= 0){
+            const nameList = this.data.map(tag => tag.name);
+            if(nameList.indexOf(name) < 0){
+                const tag = this.data.filter(tag => tag.id === id)[0];
+                tag.name = name;
+                this.save();
+                return 'success';
+            }else{
+                return 'duplicated';
+            }
+        }else{
+            return 'not found';
+        }
+    },
+    remove(id:string){
+        let index = -1;
+        for(let i = 0;i<this.data.length;i++){
+            if(this.data[i].id === id){
+                index = i;
+                break;
+            }
+        }
+        this.data.splice(index,1);
         this.save();
         return 'success';
     },
--- src/views/EditLabels.vue
+++ src/views/EditLabels.vue
@@ -1,15 +1,15 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"/>
+      <Icon class="leftIcon" name="left" @click="goBack"/>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <Notes field-name="标签名" placeholder="请输入标签名" :value.sync="tag.name"/>
+      <Notes field-name="标签名" placeholder="请输入标签名" :value.sync="tag.name" @update:value="updateTag"/>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -31,10 +31,23 @@ export default class EditLabels extends Vue {
     let tags = tagListModel.data;
     const tag = tags.filter(item => item.id === tagId)[0]
     if(tag){
-        this.tag = tag;
+        this.tag = JSON.parse(JSON.stringify(tag));
     }else{
         this.$router.replace("/404")
     }
+ }
+ updateTag(name:string){
+    if(this.tag){
+          tagListModel.update(this.tag.id,name);
+    }
+ }
+ remove(){
+  if(this.tag){
+          tagListModel.remove(this.tag.id)
+  }
+ }
+ goBack(){
+  this.$router.back();
  }
 }
 </script>
