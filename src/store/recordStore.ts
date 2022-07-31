c309cdbbc70ebf0821c3aa86e0703b466035ef12
loganwu <loganwu365@gmail.com>
2022-08-01 02:26:44 +0800

    模块化store；删除了models

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,8 +1,20 @@
-import recordListModel from "@/models/recordListModel";
+ 
+const recordStore = {
+    recordList: [] as RecordItem[],
+    createRecord: function(record: RecordItem){
+        record.createAt = new Date();
+        const deepClone = JSON.parse(JSON.stringify(record));
+        this.recordList.push(deepClone);
+        this.saveRecords();
+},
+    saveRecords: function(){
+        window.localStorage.setItem('recordList',JSON.stringify(this.recordList));
+    },
+    fetchRecords: function(){
+        this.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[]
+        return this.recordList;
+    }
+}
 
-export default {
-    recordList: recordListModel.fetch(),
-    createRecord: (record: RecordItem)=>{
-    recordListModel.create(record);
-}
-}
+recordStore.fetchRecords();
+export default recordStore
--- src/store/tagStore.ts
+++ src/store/tagStore.ts
@@ -1,27 +1,69 @@
-import tagListModel from "@/models/tagListModel";
+import createId from '@/lib/createId'
 
-export default {
-    //tag store
-    tagList: tagListModel.fetch(),
-    findTag: (id: string) => {
-        const tags = tagListModel.fetch();
-        return JSON.parse(JSON.stringify(tags.filter(item => item.id === id)[0]));
+const tagStore = {
+    tagList: [] as Tag[],
+    findTag: function(id: string){
+        return JSON.parse(JSON.stringify(this.tagList.filter(item => item.id === id)[0]));
     },
-    createTag: () => {
+    createTag: function(){
         const name = window.prompt("请输入标签名");
-        if (name === null) { return }
-        const message = tagListModel.create(name as string);
-        if (message === 'null') { window.alert("您输入的标签为空") }
-        if (message === 'duplicated') { window.alert("您输入的标签已存在") }
+        if (name === null) {return }
+        if(name === ''){
+            window.alert("您输入的标签为空");
+            return "null"
+        }
+        const nameList = this.tagList.map(item => item.name)
+        if(nameList.indexOf(name) >= 0){
+            window.alert("您输入的标签已存在");
+            return "duplicated"
+        }//返回字符串判断错误信息
+        const id = createId().toString();
+        const newTag: Tag = {
+            id: id,
+            name: name
+        }
+        this.tagList.push(newTag);
+        this.saveTags();
+        return 'success';
     },
     removeTag: function (id: string) {
-        tagListModel.remove(id);
-        this.tagList = tagListModel.fetch();//更新全局变量
+        let index = -1;
+        for(let i = 0;i<this.tagList.length;i++){
+            if(this.tagList[i].id === id){
+                index = i;
+                break;
+            }
+        }
+        this.tagList.splice(index,1);
+        this.saveTags();
         return 'success'
     },
     updateTag: function (id: string, name: string) {
-        const message = tagListModel.update(id, name);
-        this.tagList = tagListModel.fetch();
-        return message
+        const idList = this.tagList.map(tag => tag.id);
+        if(idList.indexOf(id) >= 0){
+            const nameList = this.tagList.map(tag => tag.name);
+            if(nameList.indexOf(name) < 0){
+                const tag = this.tagList.filter(tag => tag.id === id)[0];
+                tag.name = name;
+                this.saveTags();
+                return 'success';
+            }else{
+                window.alert("此标签已存在");
+                return 'duplicated';
+            }
+        }else{
+            return 'not found';
+        }
+    },
+    fetchTags: function(){
+        this.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]') as Tag[]
+        return this.tagList;
+    },
+    saveTags: function(){
+        window.localStorage.setItem('tagList',JSON.stringify(this.tagList));
     }
 }
+
+tagStore.fetchTags();
+
+export default tagStore
