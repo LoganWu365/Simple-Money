15188d5e3dd59815626e22d7ca471bf618be8d39
loganwu <loganwu365@gmail.com>
2022-08-06 22:44:44 +0800

    增加constants文件夹，储存复用常量，在Statistics.vue中改为使用常量

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,11 @@
 <template>
   <Layout>
-    <Tabs :value.sync="type" classPrefix="hhh" :data-source="typeList" />
-    <Tabs :value.sync="interval" :data-source="intervalList" />
+    <Tabs :value.sync="type" classPrefix="type" :data-source="typeList" />
+    <Tabs
+      :value.sync="interval"
+      classPrefix="interval"
+      :data-source="intervalList"
+    />
   </Layout>
 </template>
 
@@ -9,6 +13,8 @@
 import Vue from "vue";
 import Tabs from "@/components/Tabs.vue";
 import { Component } from "vue-property-decorator";
+import TypeList from "@/constants/TypeList";
+import IntervalList from "@/constants/IntervalList";
 @Component({
   components: { Tabs },
 })
@@ -16,26 +22,24 @@ export default class Statistics extends Vue {
   type = "-";
   interval = "day";
 
-  typeList = [
-    { text: "支出", value: "-" },
-    { text: "收入", value: "+" },
-  ];
-  intervalList = [
-    { text: "按天", value: "day" },
-    { text: "按周", value: "week" },
-    { text: "按月", value: "month" },
-  ];
+  typeList = TypeList;
+  intervalList = IntervalList;
 }
 </script>
 
 <style lang="scss" scoped>
-::v-deep .hhh-tabs-item {
-  background: white;
-  &.selected {
-    background: #c4c4c4;
-    &::after {
-      display: none;
+::v-deep {
+  .type-tabs-item {
+    background: white;
+    &.selected {
+      background: #c4c4c4;
+      &::after {
+        display: none;
+      }
     }
+  }
+  .tabs > .interval-tabs-item {
+    height: 48px;
   }
 }
 </style>
