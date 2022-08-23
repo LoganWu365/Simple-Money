df51848823f92f99c3dd75c165d604adfe302717
loganwu <loganwu365@gmail.com>
2022-08-23 15:01:23 +0800

    配置element-ui

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import router from './router'
 import store from './store'
 import Layout from '@/components/Layout.vue'
 import Icon from '@/components/Icon.vue'
-
+import {DatePicker} from 'element-ui'
 
 Vue.component('Layout',Layout)
 Vue.component('Icon',Icon)
 Vue.config.productionTip = false
 
+Vue.use(DatePicker)
 
 new Vue({
   router,
