9b0a023320482eeae85cd2d566a3f57a72ffe3cb
loganwu <loganwu365@gmail.com>
2022-08-28 19:01:55 +0800

    更改配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-var-requires': 0,
-    "@typescript-eslint/explicit-module-boundary-types":"off"
+    "@typescript-eslint/explicit-module-boundary-types":"off",
+    'no-undef': 0 
   },
   overrides: [
     {
